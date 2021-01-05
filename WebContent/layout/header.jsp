7868b0d4026f5f152c35b7d5fb1cd06a5b083d4e
xorud <xorud8531@gmail.com>
2021-01-05 17:38:37 +0900

    JSP 블로그과정5 - 로그인, 로그아웃 기능구현

--- WebContent/layout/header.jsp
+++ WebContent/layout/header.jsp
@@ -1,5 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<!-- JSTL (JSP Standard Tag Library)
+JSP 개발을 단순화하기위한 태그 library
+
+장점
+빠른 개발 -> JSP를 단순화하는 많은 태그를 제공
+코드 재사용성 -> 다양한 페이지에서 JSTL 태그 사용 가능
+스크립틀릿 태그를 사용할 필요가 없음 (스크립틀릿 태그를 사용하지 않음) 
+-->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,19 +25,37 @@
 <body>
 
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">블로그</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-         <a class="nav-link" href="<%=request.getContextPath() %>/user?cmd=joinForm">회원가입</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<%=request.getContextPath() %>/user?cmd=loginForm">로그인</a>
-      </li>    
-    </ul>
-  </div>  
-</nav>
-<br>
+		<a class="navbar-brand" href="<%=request.getContextPath()%>/index.jsp">블로그</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#collapsibleNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<c:choose>
+			<c:when test="${sessionScope.principal != null}"> <!-- 사용자 정보 체크 -->
+				<div class="collapse navbar-collapse" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+						<li class="nav-item"><a class="nav-link"
+							href="<%=request.getContextPath()%>/board?cmd=saveForm">글쓰기</a></li>
+						<li class="nav-item"><a class="nav-link"
+							href="<%=request.getContextPath()%>/user?cmd=updateForm">회원정보</a>
+						</li>
+						<li class="nav-item"><a class="nav-link"
+							href="<%=request.getContextPath()%>/user?cmd=logout">로그아웃</a>
+						</li>
+					</ul>
+				</div>
+			</c:when>
+			<c:otherwise>
+				<div class="collapse navbar-collapse" id="collapsibleNavbar">
+					<ul class="navbar-nav">
+						<li class="nav-item"><a class="nav-link"
+							href="<%=request.getContextPath()%>/user?cmd=joinForm">회원가입</a></li>
+						<li class="nav-item"><a class="nav-link"
+							href="<%=request.getContextPath()%>/user?cmd=loginForm">로그인</a></li>
+					</ul>
+				</div>
+			</c:otherwise>
+		</c:choose>
+
+	</nav>
+	<br> 
--- src/com/cos/blog/web/UserController.java
+++ src/com/cos/blog/web/UserController.java
@@ -9,7 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.cos.blog.domain.user.User;
 import com.cos.blog.domain.user.dto.JoinReqDto;
 import com.cos.blog.domain.user.dto.LoginReqDto;
 import com.cos.blog.service.UserService;
@@ -18,39 +20,49 @@ import com.cos.blog.util.Script;
 @WebServlet("/user")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public UserController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public UserController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doProcess(request, response);
 	}
 
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String cmd = request.getParameter("cmd");
 
 		UserService userService = new UserService();
 		// http://localhost:8080/blog/user?cmd=loginForm
-		if(cmd.equals("loginForm")) {
+		if (cmd.equals("loginForm")) {
 			// 서비스 호출
 			response.sendRedirect("user/loginForm.jsp");
-		}else if(cmd.equals("login")) {
+		} else if (cmd.equals("login")) {
 			// 서비스 호출
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
 			LoginReqDto dto = new LoginReqDto();
 			dto.setUsername(username);
 			dto.setPassword(password);
-			userService.로그인(dto);
-		}else if(cmd.equals("joinForm")) {
+			User userEntity = userService.로그인(dto);
+			if (userEntity != null) {
+				HttpSession session = request.getSession();
+				session.setAttribute("principal", userEntity); // 인증주체
+				response.sendRedirect("index.jsp");
+			} else {
+				Script.back(response, "로그인실패");
+			}
+		} else if (cmd.equals("joinForm")) {
 			response.sendRedirect("user/joinForm.jsp");
-		}else if(cmd.equals("join")) {
+		} else if (cmd.equals("join")) {
 			// 서비스 호출
 			String username = request.getParameter("username");
 			String password = request.getParameter("password");
@@ -61,25 +73,29 @@ public class UserController extends HttpServlet {
 			dto.setPassword(password);
 			dto.setEmail(email);
 			dto.setAddress(address);
-			System.out.println("회원가입 : "+dto);
+			System.out.println("회원가입 : " + dto);
 			int result = userService.회원가입(dto);
-			if(result == 1) {
+			if (result == 1) {
 				response.sendRedirect("index.jsp");
-			}else {
-				 Script.back(response, "회원가입 실패");
+			} else {
+				Script.back(response, "회원가입 실패");
 			}
-		} else if(cmd.equals("usernameCheck")) { //아이디 중복 검사
-			BufferedReader br = request.getReader(); //textPlain방식
+		} else if (cmd.equals("usernameCheck")) { // 아이디 중복 검사
+			BufferedReader br = request.getReader(); // textPlain방식
 			String username = br.readLine();
 			System.out.println(username);
 			int result = userService.유저네임중복체크(username);
-			PrintWriter out = response.getWriter(); //ok값 응답
-			if(result == 1) {
+			PrintWriter out = response.getWriter(); // ok값 응답
+			if (result == 1) {
 				out.print("ok");
-			}else {
+			} else {
 				out.print("fail");
 			}
 			out.flush();
+		} else if (cmd.equals("logout")) {
+			HttpSession session = request.getSession();
+			session.invalidate();
+			response.sendRedirect("index.jsp");
 		}
 	}
 
